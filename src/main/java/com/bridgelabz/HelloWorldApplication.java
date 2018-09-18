11d4866f730bc5018a66e052ba219c7edca70fa6
Chaitra111 <111chaitra@gmail>
2018-09-18 15:40:39 +0530

    commited first program

---
+++ src/main/java/com/bridgelabz/HelloWorldApplication.java
@@ -0,0 +1,22 @@
+package com.bridgelabz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class HelloWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldApplication.class, args);
+	}
+	
+	@GetMapping("/")
+	public String getMessage() {
+		return "Hello World";
+		
+	}
+	
+}
